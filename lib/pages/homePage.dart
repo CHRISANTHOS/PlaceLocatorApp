22bc00d266f64c1f6ef703593c3bb365631cff08
CHRISANTHOS <uakuchie@gmail.com>
2023-01-11 18:49:21 +0100

    Displayed google maps on stack widget

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,11 +1,36 @@
 import 'dart:async';
-
+import 'package:place_locator/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
   Completer<GoogleMapController> _controller = Completer();
+
+  Location location = Location();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print(location.latitude);
+  }
+
+  void _onMapCreated(GoogleMapController controller) async {
+    _controller.complete(controller);
+    await location.getCurrentLocation();
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+            target: LatLng(location.latitude!, location.longitude!), zoom: 14),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +38,10 @@ class HomePage extends StatelessWidget {
       body: Stack(
         children: [
           GoogleMap(
-            onMapCreated: (GoogleMapController controller){
-              _controller.complete(controller);
-            },
-            initialCameraPosition: CameraPosition(
-              target: LatLng(4.903663, 7.009452),
-              zoom: 14
-            ),
+            myLocationButtonEnabled: true,
+            onMapCreated: _onMapCreated,
+            initialCameraPosition:
+                CameraPosition(target: LatLng(6.5243793, 3.3792057), zoom: 14),
           )
         ],
       ),
